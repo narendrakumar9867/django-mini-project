f0e8e9634891a758bbb7d31493b26803d1b4e427
mrnarendra9867 <narendrakumark750@gmail.com>
2025-05-02 16:47:09 +0530

    database add

--- loginSignup/loginSignup/settings.py
+++ loginSignup/loginSignup/settings.py
@@ -130,4 +130,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "botcoder12@gmail.com"
-EMAIL_HOST_PASSWORD = "-"
+EMAIL_HOST_PASSWORD = "pexe owti essc ggmw"
